0f20875db422663bbc992031544af3311209cc0f
ashish <a.asis.sigdel01@gmail.com>
2025-01-16 00:53:40 +0545

    database connect

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,12 +5,18 @@ import { Sequelize } from "sequelize-typescript";
 import { applyAssociations } from "./association";
 
 const sequelize: any = new Sequelize({
-  host: "localhost",
-  username: "root",
-  password: "rocket5%",
-  database: "exam",
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
   dialect: "mysql",
   dialectModule: require("mysql2"),
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   logging: false,
   benchmark: true,
   models: [Subject, Chapter, Question],
