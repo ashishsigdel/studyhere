62d57378a9f5f7c76fa18475cf6f3f8c967e06eb
ashish <a.asis.sigdel01@gmail.com>
2025-01-30 20:09:39 +0545

    add docker

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,6 +5,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Hello world" });
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}!!!`);
 });
