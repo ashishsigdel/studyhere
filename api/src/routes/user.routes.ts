d6110fa4712f41612cd8ea3f923ebd61beb03a0b
ashish <a.asis.sigdel01@gmail.com>
2025-03-03 19:08:18 +0545

    fix(user):again fix code

--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import * as contactController from "../controllers/user.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { adminMiddleware } from "../middlewares/admin.middleware";
 import { apiKeyMiddleware } from "../middlewares/api.middleware";
 
 const router = express.Router();
